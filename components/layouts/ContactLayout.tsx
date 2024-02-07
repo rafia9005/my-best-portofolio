5bff953450091f40d92c0e74881a1d2ba1be229f
rafia9005 <rafia9005@gmail.com>
2024-02-07 08:25:08 +0700

    fix bug gitignore

--- components/layouts/ContactLayout.tsx
+++ components/layouts/ContactLayout.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function ContactLayout() {
   return (
     <>
-      <div className="flex justify-evenly items-center h-screen">
+      <div className="flex lg:mx-0 mx-[100px] justify-evenly items-center h-[50vh] lg:h-screen">
         <div>
           <motion.h1
             whileInView={{ y: -50 }}
@@ -15,7 +15,7 @@ export default function ContactLayout() {
           <motion.div
             whileInView={{ y: -50 }}
             animate={{ y: 0 }}
-            className="mt-5 text-3xl mt-5"
+            className="mt-5 text-1xl lg:text-3xl mt-5"
           >
             <h1>Email : rafia9005@gmail.com</h1>
             <h1 className="mt-5">Phone : 082143338737</h1>
--- components/layouts/TriggerLayout.tsx
+++ components/layouts/TriggerLayout.tsx
@@ -7,24 +7,24 @@ export default function TriggerLayout() {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1.5], ["1%", "-200%"]);
+  const x = useTransform(scrollYProgress, [0, 1.5], ["1%", "-180%"]);
   return (
     <>
       <section ref={targetRef} className="relative h-[300vh]">
-        <div className="flex px-[100px] px-[200px] sticky top-0 h-screen items-center overflow-hidden">
+        <div className="flex lg:px-[100px] sticky top-0 h-screen items-center overflow-hidden">
           <motion.div style={{ x }} className="flex">
             <div>
-              <h1 className="text-9xl font-bold">1 Year Experience.</h1>
+              <h1 className="lg:text-9xl text-7xl font-bold">1 Year Experience.</h1>
               <h1 className="text-5xl mt-5">coding is an art?</h1>
             </div>
           </motion.div>
           <motion.div style={{ x }} className="flex justify-evenly items-center">
             <div>
-              <h1 className="text-9xl font-bold noto">Ahmad Rafi.</h1>
-              <h1 className="text-5xl mt-5">Full-StackDeveloper</h1>
+              <h1 className="lg:text-9xl text-7xl font-bold noto">Ahmad Rafi</h1>
+              <h1 className="lg:text-5xl text-4xl mt-5">Developer</h1>
             </div>
           </motion.div>
-          <div className="rotate-[270deg] text-4xl"><h1>MY PORTOFOLIO.</h1></div>
+          <div className="rotate-[270deg] text-4xl"><h1>PORTOFOLIO</h1></div>
         </div>
       </section>
     </>
